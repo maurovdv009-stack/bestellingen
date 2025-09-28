d6f33d7a9135779d7d13b96eea2dc1e334fc0b7c
maurovdv009-stack <maurovdv.009@gmail.com>
2025-09-28 11:55:37 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,25 @@
+import { NextResponse } from 'next/server';
 
+export function middleware(req) {
+  const basicAuth = req.headers.get('authorization');
+
+  const USERNAME = process.env.BASIC_AUTH_USER;
+  const PASSWORD = process.env.BASIC_AUTH_PASSWORD;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pass] = Buffer.from(authValue, 'base64').toString().split(':');
+
+    if (user === USERNAME && pass === PASSWORD) {
+      return NextResponse.next(); // Toegestaan
+    }
+  }
+
+  const headers = new Headers();
+  headers.set('WWW-Authenticate', 'Basic realm="Dashboard"');
+  return new NextResponse('Niet toegestaan', { status: 401, headers });
+}
+
+export const config = {
+  matcher: ['/index.html', '/dashboard/:path*'], // pas dit aan als jouw dashboard op een andere route staat
+};
